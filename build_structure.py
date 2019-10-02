8dca949294c2e0d6c59cbcea69ab492312177767
Sean Andrews <sandrews@cfa.harvard.edu>
2019-10-02 14:33:33 -0400

    organizing: all drives from yaml

--- build_structure.py
+++ build_structure.py
@@ -1,12 +1,9 @@
 import numpy as np
 import yaml
+import os
 from scipy import integrate 
 from scipy.interpolate import interp1d
 
-"""
-
-
-"""
 
 # shorthand constants
 AU = 1.49597871e13
@@ -18,23 +15,35 @@ kB = 1.38064852e-16
 PI = np.pi
 
 
+"""
+Sets up the spatial grid, outputs into appropriate RADMC3D files. 
+Configures AMR_GRID.inp, RADMC3D.inp, LINES.inp.
+"""
 class Grid:
-    def __init__(self, nr, nt, naz, rin, rout):
+    def __init__(self, modelname):
+
+        # open configuration file
+        conf = open(modelname+'.yaml')
+        config = yaml.load(conf)
+        conf.close()
+        mdir = modelname+'/'
+        grid_params = config["grid"]
+        conf_params = config["setup"]
 
         # parameters
-        self.nr = nr
-        self.ntheta = nt
-        self.nphi = 1
-        self.ncells = nr * nt * self.nphi
-        self.r_in = rin * AU
-        self.r_out = rout * AU
+        self.nr = grid_params["nr"]
+        self.ntheta = grid_params["ntheta"]
+        self.nphi = grid_params["nphi"]
+        self.ncells = self.nr * self.ntheta * self.nphi
+        self.r_in = grid_params["r_in"] * AU
+        self.r_out = grid_params["r_out"] * AU
 
         ### RADIAL GRID
 
         # define cell walls and centers
         """ not sure about the centers defined in this way... """
         self.r_walls = np.logspace(np.log10(self.r_in), np.log10(self.r_out), 
-                                   nr+1)
+                                   self.nr+1)
         self.r_centers = 0.5 * (self.r_walls[:-1] + self.r_walls[1:])
 
     
@@ -46,29 +55,29 @@ class Grid:
         # define cell walls and centers
         """ not sure about the centers defined in this way... """
         self.theta_walls = 0.5*PI + po - np.logspace(np.log10(po), 
-                           np.log10(0.5*PI+po), nt+1)[::-1]
+                           np.log10(0.5*PI+po), self.ntheta+1)[::-1]
         self.theta_centers = 0.5*(self.theta_walls[:-1]+self.theta_walls[1:])
 
 
-        ### PHI GRID (= azimuth)
+        ### PHI GRID (= azimuth) : here fixed to enforce axisymmetry
 
         # define cell walls and centers
         self.phi_walls = np.array([0.0, 0.0])
         self.phi_centers = np.array([0.0])
 
 
-        ### OUTPUT
+        ### OUTPUT SPATIAL GRID
 
         # open file
-        f = open('amr_grid.inp', 'w')
+        f = open(mdir+'amr_grid.inp', 'w')
 
         # file header
-        f.write('1\n')	# format code
-        f.write('0\n')	# regular grid
+        f.write('1\n')		# format code
+        f.write('0\n')		# regular grid
         f.write('100\n')	# spherical coordinate system
-        f.write('0\n')	# no grid info written to file (as recommended)
-        f.write('1 1 0\n')	# 
-        f.write('%d %d %d\n' % (nr, nt, self.nphi))
+        f.write('0\n')		# no grid info written to file (as recommended)
+        f.write('1 1 0\n')	# axisymmetric coding
+        f.write('%d %d %d\n' % (self.nr, self.ntheta, self.nphi))
 
         # write wall coordinates to file
         for r in self.r_walls: f.write('%.9e\n' % r)
@@ -79,15 +88,73 @@ class Grid:
         f.close()
 
 
+        ### OUTPUT RT CONFIG FILE
+
+        # open file
+        f = open(mdir+'radmc3d.inp', 'w')
+
+        # configuration contents
+        f.write('incl_dust=%d\n' % conf_params["incl_dust"])
+        f.write('incl_lines=%d\n' % conf_params["incl_lines"])
+        f.write('incl_freefree=%d\n' % conf_params["incl_freefree"])
+        if conf_params["scattering"] == 'None':
+            f.write('scattering_mode_max=%d\n' % 0)
+        elif conf_params["scattering"] == 'Isotropic':
+            f.write('scattering_mode_max=%d\n' % 1)
+            f.write('nphot_scat=2000000\n')
+        elif conf_params["scattering"] == 'HG':
+            f.write('scattering_mode_max=%d\n' % 2)
+            f.write('nphot_scat=10000000\n')
+        elif conf_params["scattering"] == 'Mueller':
+            f.write('scattering_mode_max=%d\n' % 3)
+            f.write('nphot_scat=100000000\n')
+        if conf_params["binary"]:
+            f.write('writeimage_unformatted = 1\n')
+            f.write('rto_single = 1\n')
+            f.write('rto_style = 3\n')
+        else:
+            f.write('rto_style = 1\n')
+        if conf_params["camera_tracemode"]=='image':
+            f.write('camera_tracemode = 1\n')
+        elif conf_params["camera_tracemode"]=='tau':
+            f.write('camera_tracemode = -2\n')
+        if conf_params["lines_mode"]=='LTE':
+            f.write('lines_mode = 1\n')
+        f.close()
+
+
+        ### WAVELENGTH GRID
+        f = open(mdir+'wavelength_micron.inp', 'w')
+        f.write('100\n')
+        for w in np.logspace(-1, 4, 100): f.write('%e\n' % w)
+        f.close()
+
+
+        ### LINE DATA CONFIG FILE
+        if (conf_params["incl_lines"] == 1):
+           f = open(mdir+'lines.inp', 'w')
+           f.write('2\n1\n')
+           f.write('%s    leiden    0    0    0' % conf_params["molecule"]) 
+           f.close()
+
+           # copy appropriate molecular data file
+           os.system('cp moldata/'+conf_params["molecule"]+'.dat ' + mdir + \
+                     'molecule_'+conf_params["molecule"]+'.inp')
+
+
+
 class DiskModel:
-    def __init__(self, configfile):
+    def __init__(self, modelname):
 
         # open configuration file
-        conf = open(configfile)
+        conf = open(modelname+'.yaml')
         config = yaml.load(conf)
         conf.close()
+        self.mdir = modelname+'/'
         disk_params = config["disk_params"]
         host_params = config["host_params"]
+        self.do_dust = config["setup"]["incl_dust"]
+        self.do_gas = config["setup"]["incl_lines"]
 
         # stellar properties
         self.Mstar = host_params["M_star"] * Msun
@@ -235,26 +302,28 @@ class DiskModel:
     def write_Model(self, Grid):
         
         # file headers
-        #dustdens_inp = open('dust_density.inp', 'w')
-        #dustdens_inp.write('1\n%d\n1\n' % Grid.ncells)
+        if (self.do_dust == 1):
+            dustdens_inp = open(self.mdir+'dust_density.inp', 'w')
+            dustdens_inp.write('1\n%d\n1\n' % Grid.ncells)
 
-        #dusttemp_inp = open('dust_temperature.dat', 'w')
-        #dusttemp_inp.write('1\n%d\n1\n' % Grid.ncells)
+            dusttemp_inp = open(self.mdir+'dust_temperature.dat', 'w')
+            dusttemp_inp.write('1\n%d\n1\n' % Grid.ncells)
 
-        gasdens_inp = open('gas_density.inp', 'w')
-        gasdens_inp.write('1\n%d\n' % Grid.ncells)
+        if (self.do_gas == 1):
+            gasdens_inp = open(self.mdir+'gas_density.inp', 'w')
+            gasdens_inp.write('1\n%d\n' % Grid.ncells)
 
-        gastemp_inp = open('gas_temperature.inp', 'w')
-        gastemp_inp.write('1\n%d\n' % Grid.ncells)
+            gastemp_inp = open(self.mdir+'gas_temperature.inp', 'w')
+            gastemp_inp.write('1\n%d\n' % Grid.ncells)
 
-        codens_inp = open('numberdens_co.inp', 'w')
-        codens_inp.write('1\n%d\n' % Grid.ncells)
+            codens_inp = open(self.mdir+'numberdens_co.inp', 'w')
+            codens_inp.write('1\n%d\n' % Grid.ncells)
 
-        vel_inp = open('gas_velocity.inp', 'w')
-        vel_inp.write('1\n%d\n' % Grid.ncells)
+            vel_inp = open(self.mdir+'gas_velocity.inp', 'w')
+            vel_inp.write('1\n%d\n' % Grid.ncells)
 
-        turb_inp = open('microturbulence.inp', 'w')
-        turb_inp.write('1\n%d\n' % Grid.ncells)
+            turb_inp = open(self.mdir+'microturbulence.inp', 'w')
+            turb_inp.write('1\n%d\n' % Grid.ncells)
 
         # populate files
         for phi in Grid.phi_centers:
@@ -263,19 +332,24 @@ class DiskModel:
                     r_cyl = r * np.sin(theta)
                     z = r * np.cos(theta)
 
-        #            dusttemp_inp.write('%.6e\n' % self.Temp(r_cyl, z))
-                    gastemp_inp.write('%.6e\n' % self.Temp(r_cyl, z))
-        #            dustdens_inp.write('%.6e\n' % self.rho_d(r_cyl, z))
-                    gasdens_inp.write('%.6e\n' % self.rho_g(r_cyl, z))
-                    codens_inp.write('%.6e\n' % self.nCO(r_cyl, z))
-                    vel_inp.write('0 0 %.6e\n' % self.velocity(r_cyl))
-                    turb_inp.write('%.6e\n' % self.vturb(r_cyl, z))
+                    if (self.do_dust == 1):
+                        dusttemp_inp.write('%.6e\n' % self.Temp(r_cyl, z))
+                        dustdens_inp.write('%.6e\n' % self.rho_d(r_cyl, z))
+
+                    if (self.do_gas == 1):
+                        gastemp_inp.write('%.6e\n' % self.Temp(r_cyl, z))
+                        gasdens_inp.write('%.6e\n' % self.rho_g(r_cyl, z))
+                        codens_inp.write('%.6e\n' % self.nCO(r_cyl, z))
+                        vel_inp.write('0 0 %.6e\n' % self.velocity(r_cyl))
+                        turb_inp.write('%.6e\n' % self.vturb(r_cyl, z))
 
         # close files
-        gastemp_inp.close()
-        gasdens_inp.close()
-        #dusttemp_inp.close()
-        #dustdens_inp.close()
-        codens_inp.close()
-        vel_inp.close()
-        turb_inp.close()
+        if (self.do_dust == 1):
+            dusttemp_inp.close()
+            dustdens_inp.close()
+        if (self.do_gas == 1):
+            gastemp_inp.close()
+            gasdens_inp.close()
+            codens_inp.close()
+            vel_inp.close()
+            turb_inp.close()
--- test_build.py
+++ test_build.py
@@ -6,78 +6,40 @@ import build_structure as model
 import matplotlib.pyplot as plt
 from fitsconversion import convert_to_fits
 
-def print_radmc_inp(incl_dust=0, incl_lines=1, incl_freefree=0, 
-                    scattering='Isotropic', binary=False, 
-                    camera_tracemode='image', lines_mode='LTE'):
 
-    filename = 'radmc3d.inp'
-    f = open(filename, 'w')
-    f.write('incl_dust=%d\n' % incl_dust)
-    f.write('incl_lines=%d\n' % incl_lines)
-    f.write('incl_freefree=%d\n' % incl_freefree)
-    if scattering == 'None':
-        f.write('scattering_mode_max=%d\n' % 0)
-    elif scattering == 'Isotropic':
-        f.write('scattering_mode_max=%d\n' % 1)
-        f.write('nphot_scat=2000000\n')
-    elif scattering == 'HG':
-        f.write('scattering_mode_max=%d\n' % 2)
-        f.write('nphot_scat=10000000\n')
-    elif scattering == 'Mueller':
-        f.write('scattering_mode_max=%d\n' % 3)
-        f.write('nphot_scat=100000000\n')
-    if binary:
-        f.write('writeimage_unformatted = 1\n')
-        f.write('rto_single = 1\n')
-        f.write('rto_style = 3\n')
-    else:
-        f.write('rto_style = 1\n')
-
-    if camera_tracemode=='image':
-        f.write('camera_tracemode = 1\n')
-    elif camera_tracemode=='tau':
-        f.write('camera_tracemode = -2\n')
-    if lines_mode=='LTE':
-        f.write('lines_mode = 1\n')
-
-    f.close()
+# parameter file and bookkeeping setup
+modelname = 'testmodel'
+if not os.path.exists(modelname):
+    os.makedirs(modelname)
 
 
+# set up the spatial grid and RADMC3D config files
+grid = model.Grid(modelname)
 
-# load setup file and parameters
-setup_file = 'testmodel.yaml'
-conf = open(setup_file)
-config = yaml.load(conf)
-conf.close()
 
-# set up the spatial grid
-grid = config["grid"]
-nr = grid["nr"]
-ntheta = grid["ntheta"]
-nphi = grid["nphi"]
-r_in = grid["r_in"]
-r_out = grid["r_out"]
-grid = model.Grid(nr, ntheta, nphi, r_in, r_out)
-
-# create a model structure and write the RADMC3D input files
-diskmodel = model.DiskModel(setup_file)
-print_radmc_inp(incl_dust = 0, incl_lines = 1)
+# build a model structure 
+diskmodel = model.DiskModel(modelname)
 diskmodel.write_Model(grid)
 
 
-# make a set of channel maps
-incl, PA, npix = 50., 320., 750.
-#os.system('radmc3d image iline 2 widthkms 2.0 linenlam 10 incl %.2f posang %.2f npix %d setthreads 4' % (incl, PA, npix))
+# extract additional parameters for synthetic observations
+conf = open(modelname+'.yaml')
+pars = yaml.load(conf)
+conf.close()
 
+
+# raytracing
+os.chdir(modelname)
 os.system('rm -rf image.out')
-os.system('radmc3d image incl 50 posang 30 npix 750 iline 2 imolspec 1 widthkms 2.0 vkms 0.0 linenlam 15 setthreads 4')
-convert_to_fits('image.out', 'test.fits', 140., RA=165.5, DEC=-34.7, tau=False)
+os.system("radmc3d image incl %.2f posang %.2f " % \
+          (pars["outputs"]["incl"], pars["outputs"]["PA"]) + \
+          "npix %d iline %d " % \
+          (pars["outputs"]["npix"], pars["setup"]["transition"]) + \
+          "widthkms %.2f linenlam %d setthreads 4" % \
+          (pars["outputs"]["widthkms"], pars["outputs"]["nchan"]))
 
-AU = 1.49597871e13
-Msun = 1.98847542e33
-mu_gas = 2.37
-m_H = 1.67353284e-24
-G = 6.67408e-8
-kB = 1.38064852e-16
-PI = np.pi
 
+# make a FITS file
+convert_to_fits('image.out', modelname+'_'+pars["setup"]["molecule"]+'.fits', 
+                pars["outputs"]["dpc"], RA=pars["outputs"]["RA"], 
+                DEC=pars["outputs"]["DEC"], tau=False)
