6eb685927538d16b105eb74ab67de11a4cc4dd37
Sean Andrews <sandrews@cfa.harvard.edu>
2019-12-17 11:04:44 -0500

    raytracing setups

--- generate_model.py
+++ generate_model.py
@@ -7,46 +7,26 @@ from grid import grid
 from disk import disk
 from plotutils import plotutils
 from fitsconversion import convert_to_fits
+from raytrace_maps import raytrace_maps
 
 
 model_name = 'demo'
 
 
 # generate the spatial grid
-sim_grid = grid(model_name)
+#sim_grid = grid(model_name)
 
 
 # generate a model structure on that grid
-print('making structure')
-t0 = time.time()
-sim_disk = disk(model_name, sim_grid)
-print(time.time()-t0)
+#print('making structure')
+#t0 = time.time()
+#sim_disk = disk(model_name, sim_grid)
+#print(time.time()-t0)
 
-print('plotting structures')
-_ = plotutils(model_name)
-
-
+#print('plotting structures')
+#_ = plotutils(model_name)
 
 
-
-# raytracing
+# raytrace out a set of channel maps
 print('raytracing')
-conf = open(model_name + ".yaml")
-config = yaml.load(conf, Loader=yaml.FullLoader)
-outpars = config["outputs"]
-setpars = config["setup"]
-conf.close()
-os.chdir(model_name)
-os.system('rm -rf image.out')
-os.system("radmc3d image incl %.2f posang %.2f " % \
-          (outpars["incl"], outpars["PA"]) + \
-          "npix %d iline %d " % \
-          (outpars["npix"], setpars["transition"]) + \
-          "widthkms %.2f linenlam %d setthreads 4" % \
-          (outpars["widthkms"], outpars["nchan"]))
-
-# make a FITS file
-outfile = model_name+'_'+setpars["molecule"]+'.fits'
-os.system('rm '+outfile)
-convert_to_fits('image.out', outfile, outpars["dpc"],
-                RA=outpars["RA"], DEC=outpars["DEC"])
+ch_maps = raytrace_maps(model_name)
---
+++ raytrace_maps.py
@@ -0,0 +1,57 @@
+import numpy as np
+import sys
+import os
+import yaml
+from fitsconversion import convert_to_fits
+
+
+class raytrace_maps:
+
+    def __init__(self, modelname, writefits=True):
+
+        # load information
+        conf = open(modelname + ".yaml")
+        config = yaml.load(conf, Loader=yaml.FullLoader)
+        outpars = config["outputs"]
+        setpars = config["setup"]
+        gridpars = config["grid"]
+        conf.close()
+
+        # position angle convention (for now)
+        posang = outpars["geometry"]["PA"] - 90.
+
+        # specify pixel resolution, use 10% buffer over model grid size
+        sizeau = 1.1 * 2 * gridpars["spatial"]["r_max"]
+        npix = sizeau / outpars["geometry"]["dpc"] / outpars["spatial"]["ires"]
+        npix = np.int(np.ceil(npix / 2) * 2)
+
+
+        # velocity scale
+        cc = 2.99792458e10
+        nu0 = 230.538e9
+
+        DV0 = 2 * 1e5 * outpars["velocity"]["widthkms"]
+        dnu = 1e3 * outpars["velocity"]["dfreq"]	
+        nchan = np.int(np.ceil(DV0 * nu0 / cc / dnu))
+        widthkms = (nchan - 1) * cc * dnu / nu0 / 1e5 / 2
+
+        # run raytracer
+        os.chdir(modelname)
+        os.system('radmc3d image ' + \
+                  'incl %.2f ' % outpars["geometry"]["incl"] + \
+                  'posang %.2f ' % posang + \
+                  'npix %d ' % npix + \
+                  'sizeau %d ' % sizeau + \
+                  'iline %d ' % setpars["transition"] + \
+                  'widthkms %.5f ' % widthkms + \
+                  'linenlam %d ' % nchan + \
+                  'setthreads 4')
+
+
+        # make a FITS cube
+        if writefits:
+            outfile = modelname + '_' + setpars["molecule"] + '.fits'
+            os.system('rm ' + outfile)
+            convert_to_fits('image.out', outfile, outpars["geometry"]["dpc"], 
+                            RA=outpars["spatial"]["RA"], 
+                            DEC=outpars["spatial"]["DEC"])
