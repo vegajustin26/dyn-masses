c24a37ea08003b5f6a8d03c89e908ad3b41ddad2
Sean Andrews <sandrews@cfa.harvard.edu>
2020-05-06 16:38:12 -0400

    cleaning up analysis plots

--- simple_chanmaps/analyze_fit.py
+++ simple_chanmaps/analyze_fit.py
@@ -22,15 +22,10 @@ theta = [30., 40., 1.75, 150., -0.5, 300., 0., 0., 0.]
 # parse the samples
 all_samples = reader.get_chain(discard=0, flat=False)
 samples = reader.get_chain(discard=burnin, flat=False)
-fsamples = reader.get_chain(discard=burnin, flat=True)
 nsteps, nwalk, ndim = samples.shape[0], samples.shape[1],  samples.shape[2]
 
-# parse the probabilities
-lnprob = reader.get_log_prob(discard=burnin, flat=False)
-flnprob = reader.get_log_prob(discard=burnin, flat=True)
-
 # plot the integrated autocorrelation time convergence every Ntau steps
-Ntau = 50
+Ntau = 100
 Nmax = all_samples.shape[0]
 if (Nmax > Ntau):
     tau_ix = np.empty(np.int(Nmax / Ntau))
@@ -44,19 +39,23 @@ if (Nmax > Ntau):
     fig = plt.figure()
     plt.plot(ix, tau_ix, '-o')
     plt.xlabel('steps')
-    plt.ylabel('autocorr time')
+    plt.ylabel('autocorr time (steps)')
     plt.xlim([0, Nmax])
     plt.ylim([0, tau_ix.max() + 0.1 * (tau_ix.max() - tau_ix.min())])
     fig.savefig(fname+'.autocorr.png')
     fig.clf()
 
 # plot the traces
-fig = plt.figure()
+fig = plt.figure(figsize=(6, 3.5))
 for idim in np.arange(ndim):
     plt.subplot(3, 3, idim+1)
     for iw in np.arange(nwalk):
         plt.plot(np.arange(nsteps), samples[:, iw, idim], alpha=0.3)
-        plt.ylabel(lbls[idim])
+        plt.ylabel(lbls[idim], fontsize=6)
+        plt.xticks(fontsize=6)
+        plt.yticks(fontsize=6)
+fig.subplots_adjust(wspace=0.45, hspace=0.3)
+fig.subplots_adjust(left=0.10, right=0.90, bottom=0.04, top=0.99)
 fig.savefig(fname + '.traces.png')
 fig.clf()
 
